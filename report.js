108b388eb684805fcc7ff40e052f587ba33574e0
Richard van der Dys <richard.vanderdys@gmail.com>
2018-05-22 12:35:17 +0300

    Fixed reporting bug

--- report.js
+++ report.js
@@ -1,6 +1,8 @@
 const jsonSql = require('json-sql')({ dialect: 'postgresql', namedValues: false });
 const { Client } = require('pg');
 const http = require('http');
+const debug = require('debug');
+const log = debug('Watson Temp Reporter');
 
 const tableQuery = `
 CREATE TABLE IF NOT EXISTS temps (
@@ -30,7 +32,9 @@ const report = ({ temp, time, location }) => {
     }
   });
   const url = `${process.env.REPORTING_SERVER}/temp/${temp}/${location}/${time}`;
-  http.get(url);
+  http.get(url).on('error', err => {
+    log('Could not report to server', err.message);
+  });
   return preformQuery(sql.query, sql.values);
 }
 
